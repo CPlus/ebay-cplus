6984f6a81bfb019d247c9c6934511477112e772b
Laszlo Bacsi <lackac@lackac.hu>
2014-07-16 16:35:32 +0200

    Automatically use wsdl in a Rails app if saved under config/

--- lib/ebay/railtie.rb
+++ lib/ebay/railtie.rb
@@ -3,6 +3,9 @@ module Ebay
     initializer "ebay.configure_api_settings" do
       path = Rails.root.join("config/ebay.yml")
       Ebay::Api.configure_from_yaml(path, Rails.env) if path.exist?
+
+      wsdl_path = Rails.root.join("config/eBaySvc.wsdl")
+      Ebay::Api.wsdl = wsdl_path if wsdl_path.exist?
     end
   end
 end
